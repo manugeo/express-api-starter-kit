d968d5dce74fa2534794bbb69c9419423133a4f5
Manu <manugeo13@gmail.com>
2025-03-11 15:39:35 +0530

    Updates to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
-import express from 'express';
+import app from './app';
 import config from './utils/config';
 import logger from './utils/logger';
-
-const app = express();
-
-app.get('/', (_req, res) => {
-  res.json({
-    message: 'Express API Starter Kit is running',
-  });
-});
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
 });
-
-module.exports = app;
